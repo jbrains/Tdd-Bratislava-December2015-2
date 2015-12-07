94b854eb37cf987c08902df46af374c21d4a377e
J. B. Rainsberger <me@jbrains.ca>
2015-12-07 16:31:32 +0100

    We can now find the price for any barcode.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -3,6 +3,9 @@ package ca.jbrains.pos.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
@@ -52,14 +55,21 @@ public class SellOneItemTest {
         }
 
         public void onBarcode(String barcode) {
-            if ("".equals(barcode))
+            final Map<String, String> pricesByBarcode = new HashMap<String, String>() {{
+                put("12345", "EUR 7.95");
+                put("23456", "EUR 12.50");
+            }};
+
+            if ("".equals(barcode)) {
                 display.setText("Scanning error: empty barcode");
-            else if ("12345".equals(barcode))
-                display.setText("EUR 7.95");
-            else if ("23456".equals(barcode))
-                display.setText("EUR 12.50");
-            else
+                return;
+            }
+
+            final String price = pricesByBarcode.get(barcode);
+            if (price == null)
                 display.setText(String.format("Product not found for %s", barcode));
+            else
+                display.setText(price);
         }
     }
 
