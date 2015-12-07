478e8549e141ecdd165e8d7175d39459b9819bc0
J. B. Rainsberger <me@jbrains.ca>
2015-12-07 14:37:20 +0100

    Tried to simplify the tests for Fraction equals()

--- src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
+++ src/test/java/ca/jbrains/math/test/FractionEqualsTest.java
@@ -9,6 +9,9 @@ import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class FractionEqualsTest {
     @Test
     public void someEqualValues() throws Exception {
@@ -53,26 +56,11 @@ public class FractionEqualsTest {
     @RunWith(Theories.class)
     public static class SymmetricProperty {
         @DataPoints
-        public static final Fraction[] notAllDifferentValues = new Fraction[]{
-                new Fraction(1, 2),
-                new Fraction(3, 4),
-                new Fraction(-7, 8),
-                new Fraction(9, -10),
-                new Fraction(5),
-                new Fraction(0),
-                new Fraction(4 * 1, 4 * 2),
-                new Fraction(4 * 3, 4 * 4),
-                new Fraction(4 * (-7), 4 * 8),
-                new Fraction(4 * 9, 4 * (-10)),
-                new Fraction(4 * 5),
-                new Fraction(4 * 0),
-                new Fraction(-7 * 1, -7 * 2),
-                new Fraction(-7 * 3, -7 * 4),
-                new Fraction(-7 * (-7), -7 * 8),
-                new Fraction(-7 * 9, -7 * (-10)),
-                new Fraction(-7 * 5),
-                new Fraction(-7 * 0),
-        };
+        public static final Fraction[] notAllDifferentValues =
+                Arrays.asList(ReflexiveProperty.allDifferentValues).stream()
+                        .flatMap((f) -> Arrays.asList(1, 4, 8, -3, -7, 1932, -392132).stream()
+                                .map((n) -> new Fraction(n * f.getNumerator(), n * f.getDenominator())))
+                        .collect(Collectors.<Fraction>toList()).toArray(new Fraction[0]);
 
         @Theory
         public void differentValues(Fraction one, Fraction differentValue) throws Exception {
@@ -86,71 +74,54 @@ public class FractionEqualsTest {
             Assume.assumeTrue(one.equals(equalToOne));
             Assert.assertTrue(equalToOne.equals(one));
         }
+    }
 
-        @RunWith(Theories.class)
-        public static class TransitiveProperty {
-            @DataPoints
-            public static final Fraction[] notAllDifferentValues = new Fraction[]{
-                    new Fraction(1, 2),
-                    new Fraction(3, 4),
-                    new Fraction(-7, 8),
-                    new Fraction(9, -10),
-                    new Fraction(5),
-                    new Fraction(0),
-                    new Fraction(4 * 1, 4 * 2),
-                    new Fraction(4 * 3, 4 * 4),
-                    new Fraction(4 * (-7), 4 * 8),
-                    new Fraction(4 * 9, 4 * (-10)),
-                    new Fraction(4 * 5),
-                    new Fraction(4 * 0),
-                    new Fraction(-7 * 1, -7 * 2),
-                    new Fraction(-7 * 3, -7 * 4),
-                    new Fraction(-7 * (-7), -7 * 8),
-                    new Fraction(-7 * 9, -7 * (-10)),
-                    new Fraction(-7 * 5),
-                    new Fraction(-7 * 0),
-            };
+    @RunWith(Theories.class)
+    public static class TransitiveProperty {
+        @DataPoints
+        public static final Fraction[] notAllDifferentValues = SymmetricProperty.notAllDifferentValues;
 
-            @Theory
-            public void holds(Fraction a, Fraction b, Fraction c) throws Exception {
-                Assume.assumeTrue(a.equals(b) && b.equals(c));
-                Assert.assertEquals(c, a);
-            }
-
-            @Theory
-            public void doesNotHoldDueToA(Fraction a, Fraction b, Fraction c) throws Exception {
-                Assume.assumeFalse(a.equals(b));
-                Assume.assumeTrue(b.equals(c));
-                Assert.assertNotEquals(c, a);
-            }
-            @Theory
-            public void doesNotHoldDueToC(Fraction a, Fraction b, Fraction c) throws Exception {
-                Assume.assumeTrue(a.equals(b));
-                Assume.assumeFalse(b.equals(c));
-                Assert.assertNotEquals(c, a);
-            }
+        @Theory
+        public void holds(Fraction a, Fraction b, Fraction c) throws Exception {
+            Assume.assumeTrue(a.equals(b) && b.equals(c));
+            Assert.assertEquals(c, a);
         }
 
-        @RunWith(Theories.class)
-        public static class DenominatorOfOne {
-            @DataPoints
-            public static final int[] numerators = new int[] { 1, 2, 4, 7, 8, -19, -329};
-
-            @Theory
-            public void nEqualsNOver1(int n) throws Exception {
-                Assert.assertEquals(new Fraction(n, 1), new Fraction(n));
-            }
+        @Theory
+        public void doesNotHoldDueToA(Fraction a, Fraction b, Fraction c) throws Exception {
+            Assume.assumeFalse(a.equals(b));
+            Assume.assumeTrue(b.equals(c));
+            Assert.assertNotEquals(c, a);
         }
 
-        @RunWith(Theories.class)
-        public static class TheManyFacesOfZero {
-            @DataPoints
-            public static final int[] denominators = new int[] { 1, 2, 4, 7, 8, -19, -329};
+        @Theory
+        public void doesNotHoldDueToC(Fraction a, Fraction b, Fraction c) throws Exception {
+            Assume.assumeTrue(a.equals(b));
+            Assume.assumeFalse(b.equals(c));
+            Assert.assertNotEquals(c, a);
+        }
+    }
 
-            @Theory
-            public void zeroEqualsZeroOverN(int n) throws Exception {
-                Assert.assertEquals(new Fraction(0), new Fraction(0, n));
-            }
+    @RunWith(Theories.class)
+    public static class DenominatorOfOne {
+        @DataPoints
+        public static final int[] numerators = new int[]{1, 2, 4, 7, 8, -19, -329};
+
+        @Theory
+        public void nEqualsNOver1(int n) throws Exception {
+            Assert.assertEquals(new Fraction(n, 1), new Fraction(n));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class TheManyFacesOfZero {
+        @DataPoints
+        public static final int[] denominators = new int[]{1, 2, 4, 7, 8, -19, -329};
+
+        @Theory
+        public void zeroEqualsZeroOverN(int n) throws Exception {
+            Assert.assertEquals(new Fraction(0), new Fraction(0, n));
         }
     }
 }
+
