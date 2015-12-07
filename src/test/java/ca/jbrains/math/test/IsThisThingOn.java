aa6f8c93203ddbeec84a38b07ff139e5c873e1cf
J. B. Rainsberger <me@jbrains.ca>
2015-12-07 10:40:46 +0100

    [FAILING TEST] We can write a test.

---
+++ src/test/java/ca/jbrains/math/test/IsThisThingOn.java
@@ -0,0 +1,11 @@
+package ca.jbrains.math.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsThisThingOn {
+    @Test
+    public void hello() throws Exception {
+        Assert.assertEquals(false, true);
+    }
+}
