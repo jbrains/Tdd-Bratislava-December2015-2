6c51cd1655898209d73e61221aeb583f2421e98a
J. B. Rainsberger <me@jbrains.ca>
2015-12-09 14:08:27 +0100

    We can process several barcodes.

--- src/test/java/ca/jbrains/pos/test/ProcessTextAsBarcodesTest.java
+++ src/test/java/ca/jbrains/pos/test/ProcessTextAsBarcodesTest.java
@@ -2,6 +2,7 @@ package ca.jbrains.pos.test;
 
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -38,13 +39,31 @@ public class ProcessTextAsBarcodesTest {
         process(barcodeScannedListener, new StringReader("::barcode::"));
     }
 
+    @Test
+    public void severalBarcodes() throws Exception {
+        final BarcodeScannedListener barcodeScannedListener
+                = context.mock(BarcodeScannedListener.class);
+
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode("::barcode 1::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 2::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 3::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 4::");
+        }});
+
+        process(barcodeScannedListener, new StringReader("::barcode 1::"
+                + "\n::barcode 2::"
+                + "\n::barcode 3::"
+                + "\n::barcode 4::"
+        ));
+    }
+
     private void process(
             BarcodeScannedListener barcodeScannedListener,
             Reader source) throws IOException {
 
-        final String line = new BufferedReader(source).readLine();
-        if (line != null)
-            barcodeScannedListener.onBarcode(line);
+        new BufferedReader(source).lines()
+                .forEach(barcodeScannedListener::onBarcode);
     }
 
     public interface BarcodeScannedListener {
