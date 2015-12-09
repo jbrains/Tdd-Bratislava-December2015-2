2533c73fa22b8fed9233a5e6f27b88d3a1fa179c
J. B. Rainsberger <me@jbrains.ca>
2015-12-09 14:21:35 +0100

    We now ignore empty lines of text.

--- src/test/java/ca/jbrains/pos/test/ProcessTextAsBarcodesTest.java
+++ src/test/java/ca/jbrains/pos/test/ProcessTextAsBarcodesTest.java
@@ -57,12 +57,32 @@ public class ProcessTextAsBarcodesTest {
                 + "\n::barcode 4::"
         ));
     }
+    @Test
+    public void someEmptyLines() throws Exception {
+        final BarcodeScannedListener barcodeScannedListener
+                = context.mock(BarcodeScannedListener.class);
+
+        context.checking(new Expectations() {{
+            oneOf(barcodeScannedListener).onBarcode("::barcode 1::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 2::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 3::");
+            oneOf(barcodeScannedListener).onBarcode("::barcode 4::");
+        }});
+
+        process(barcodeScannedListener, new StringReader("\n\n\n\n::barcode 1::"
+                + "\n\r\n\n\r\n::barcode 2::"
+                + "\n\n::barcode 3::"
+                + "\n\n\n\n\n\r\r\n\n\n\n::barcode 4::"
+                + "\n\n\r\r\r\n\n\n"
+        ));
+    }
 
     private void process(
             BarcodeScannedListener barcodeScannedListener,
             Reader source) throws IOException {
 
         new BufferedReader(source).lines()
+                .filter((line) -> !line.isEmpty())
                 .forEach(barcodeScannedListener::onBarcode);
     }
 
